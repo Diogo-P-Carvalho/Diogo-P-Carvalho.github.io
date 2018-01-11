935f331ec7b848b77b126b5b880b527d2b3e23be
Diogo Carvalho <34646795+Diogo-P-Carvalho@users.noreply.github.com>
2018-01-11 08:53:39 +0000

    Update wikipedia_viewer_js.js

--- Wikipedia Viewer/wikipedia_viewer_js.js
+++ Wikipedia Viewer/wikipedia_viewer_js.js
@@ -4,24 +4,7 @@ $(document).ready(function(){
       $("#searchButton").click();
     }
   });
-  /*$("#searchText").autocomplete({
-    source: function(request, response) {
-        $.ajax({
-            url: "http://en.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                'action': "opensearch",
-                'format': "json",
-                'search': request.term
-            },
-            success: function(data) {
-                response(data[1]);
-
-            }
-        });
-    }
-  });*/
-
+  
 });
 
 function searchWikipedia(){
